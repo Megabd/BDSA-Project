816c7a4c874137ac9e8142dbd6e9f419da667ffc
Nickromancer <nicha@itu.dk>
2022-11-09 17:24:44 +0100

    Created puml files (diagrams)

--- Project/Author.cs
+++ Project/Author.cs
@@ -3,8 +3,7 @@ public class Author
 {
     public string aName {get; set;}
     public DateTimeOffset aDate {get; set;}
-    public Author(string name, DateTimeOffset date)
-    {
+    public Author(string name, DateTimeOffset date) {
       aName = name;
       aDate = date;
     }
