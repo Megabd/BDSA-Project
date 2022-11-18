b777750dd445442e367c0de49ce646e62dabdddd
Oliver Flyckt Wilhjelm <olfw@itu.dk>
2022-11-18 10:54:22 +0100

    last changes

--- Project.Core/GitHubArchiveDTO.cs
+++ Project.Core/GitHubArchiveDTO.cs
@@ -2,4 +2,4 @@ namespace Project.Core;
 
 public record GitHubArchiveDTO(int Id, string RepositoryName, DateTimeOffset LatestCommit);
 public record CreateGitHubArchiveDTO(string RepositoryName);
-public record UpdateGitHubArchiveDTO(string RepositoryName, DateTimeOffset LatestCommit);
+public record UpdateGitHubArchiveDTO(int Id, DateTimeOffset LatestCommit);
