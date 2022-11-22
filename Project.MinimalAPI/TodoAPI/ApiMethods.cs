a860c32219d70bdb1a3a157df2fa6fcd3cb46f90
adamjhr <adam.jh.rose6@gmail.com>
2022-11-22 13:18:40 +0100

    Pull repos already in temp

--- Project.MinimalAPI/TodoAPI/ApiMethods.cs
+++ Project.MinimalAPI/TodoAPI/ApiMethods.cs
@@ -6,10 +6,20 @@ public class ApiMethods {
     public static string CloneRepo(string repoOwner, string repoName) {
         var url = "https://github.com/"+repoOwner+"/"+repoName;
         var path = @"..\temp\"+repoOwner+@"\"+repoName;
-        if (!Directory.Exists(path)) {
-            var repo = Repository.Clone(url, path, new CloneOptions());
-        }
         var totalPath = path+@"\.git";
+        if (!Directory.Exists(path)) {
+            Repository.Clone(url, path, new CloneOptions());
+        } else {
+            var repo = new Repository(totalPath);
+            var pullOptions = new PullOptions() {
+                MergeOptions = new MergeOptions()
+                {
+                    FastForwardStrategy = FastForwardStrategy.Default
+                }
+            };
+            var signature = new Signature("_", "_", DateTimeOffset.Now);
+            Commands.Pull(repo, signature, pullOptions);
+        }
         return totalPath;
     }
 
